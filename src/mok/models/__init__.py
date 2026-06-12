8e48f27025c6dbe913ec0248cb3b906401783f7b
Shawn <106923464+nawnie@users.noreply.github.com>
2026-06-12 19:51:34 -0400

    Add MOK models package

---
+++ src/mok/models/__init__.py
@@ -0,0 +1,5 @@
+"""Model registry primitives for MOK-managed expert assets."""
+
+from mok.models.registry import AssetState, ExpertMetadata, ModelRegistry
+
+__all__ = ["AssetState", "ExpertMetadata", "ModelRegistry"]
